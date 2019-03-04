9d6edc3c5fe39aabba6c4e3edb1d44a7d9f56ae0
Banee Ishaque K <baneeishaque@gmail.com>
2019-03-04 12:52:25 +0530

    integrated nabeel work

---
+++ app/src/main/java/ndk/nikhil/gym_class_accounter/nabeel/Main2Activity.java
@@ -0,0 +1,15 @@
+package ndk.nikhil.gym_class_accounter.nabeel;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ndk.nikhil.gym_class_accounter.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nabeel_activity_main2);
+    }
+}
