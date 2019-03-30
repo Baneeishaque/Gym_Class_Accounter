e6c34afcc8840dd11777f05dfed4eee713ed1fef
Banee Ishaque K <baneeishaque@gmail.com>
2019-03-30 14:22:11 +0530

    Last

--- app/src/main/java/ndk/nikhil/gym_class_accounter/activities/Activity_Member_Profile.java
+++ app/src/main/java/ndk/nikhil/gym_class_accounter/activities/Activity_Member_Profile.java
@@ -1,15 +1,15 @@
-package ndk.nikhil.gym_class_accounter.recyclerview_items;
+package ndk.nikhil.gym_class_accounter.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import ndk.nikhil.gym_class_accounter.R;
 
-public class RecyclerView_Item_Member extends AppCompatActivity {
+public class Activity_Member_Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.recyclerview_item_member);
+        setContentView(R.layout.activity_member_profile);
     }
 }
